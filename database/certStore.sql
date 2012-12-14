9519da54ab4f506335b7ee2ab04e8f5ef6a84f60
root <root@debian.(none)>
2012-12-14 00:57:48 +0000

    changed database declaration.

--- database/certStore.sql
+++ database/certStore.sql
@@ -3,8 +3,7 @@
 CREATE TABLE certs (
   serialNumber varchar(255) NOT NULL,
   issuerName varchar(255) NOT NULL,
-  domainName varchar(255) NOT NULL,
-  issuerName varchar(255) NOT NULL,
+  subjectName varchar(255) NOT NULL,
   notBefore varchar(255) NOT NULL,
   notAfter varchar(255) NOT NULL,
   timesChecked int,
